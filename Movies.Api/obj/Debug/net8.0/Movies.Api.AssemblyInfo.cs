17d94b73d2ac28dab77f789ba1b32bd5eae085a9
Behruz Saifov <behruz.saifov.99@gmail.com>
2024-11-28 22:28:14 +0500

    own pc work

--- Movies.Api/obj/Debug/net8.0/Movies.Api.AssemblyInfo.cs
+++ Movies.Api/obj/Debug/net8.0/Movies.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ca1c22585a840d24f299a64b8c7a392f6e73206")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2feb71e88487859b68b2132031e4158c92ec2c36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
