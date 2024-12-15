3a0b9c1ebc5832bf4170bae56cf4ec1fff76792c
Behruz Saifov <behruz.saifov.99@gmail.com>
2024-12-15 21:52:34 +0500

    Delete rating

--- Movies.Application/Repositories/IRatingRepository.cs
+++ Movies.Application/Repositories/IRatingRepository.cs
@@ -9,4 +9,6 @@ public interface IRatingRepository
     Task<float?> GetRatingAsync(Guid movieId, CancellationToken token = default);
 
     Task<(float? Rating, int? UserRating)> GetRatingAsync(Guid movieId, Guid userId, CancellationToken token = default);
+    
+    Task<bool> DeleteRatingAsync(Guid movieId, Guid userId, CancellationToken token = default);
 }
