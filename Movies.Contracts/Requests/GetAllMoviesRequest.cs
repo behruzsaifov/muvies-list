309e10ab9e9f7130ca3a6d9ad1582afb7a35e5be
behruzsaifov <behruz.saifov.99@gmail.com>
2024-12-17 10:22:03 +0500

    Sorting

--- Movies.Contracts/Requests/GetAllMoviesRequest.cs
+++ Movies.Contracts/Requests/GetAllMoviesRequest.cs
@@ -7,4 +7,6 @@ public class GetAllMoviesRequest
     public required string? Title { get; init; }
     
     public required int? Year { get; init; }
+    
+    public required string? SortBy { get; init; }
 }
