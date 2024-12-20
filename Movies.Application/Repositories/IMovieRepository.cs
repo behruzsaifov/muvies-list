f03999769159b1e891c90fb77e64dcae8c461579
behruzsaifov <behruz.saifov.99@gmail.com>
2024-12-20 10:16:47 +0500

    delete empty lines

--- Movies.Application/Repositories/IMovieRepository.cs
+++ Movies.Application/Repositories/IMovieRepository.cs
@@ -12,5 +12,4 @@ public interface IMovieRepository
     Task<bool> DeleteByIdAsync(Guid id, CancellationToken token = default);
     Task<bool> ExistsByIdAsync(Guid id, CancellationToken token = default);
     Task<int> GetCountAsync(string? title, int? yearOfRelease, CancellationToken token = default);
-
 }
