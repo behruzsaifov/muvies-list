b993ec0a6f27325c5e1bbf336727ff9f97dbd837
behruzsaifov <behruz.saifov.99@gmail.com>
2024-12-24 10:49:57 +0500

    Using refit in Consumer

--- Movies.Api.Sdk.Consumer/Program.cs
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
+using Movies.Api.Sdk;
+using Refit;
 
-Console.WriteLine("Hello, World!");
+var moviesApi = RestService.For<IMoviesApi>("https://localhost:7163");
+
+var movie = await moviesApi.GetMoviesAsync("nick-the-greek-2023");
+
+Console.WriteLine(JsonSerializer.Serialize(movie));
