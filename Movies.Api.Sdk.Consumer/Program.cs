8d6aa972d60d060e4cd4b4dfa17d6236d2e01e42
Behruz Saifov <behruz.saifov.99@gmail.com>
2024-12-24 21:25:01 +0500

    Implementing Refit to Sdk

--- Movies.Api.Sdk.Consumer/Program.cs
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -1,9 +1,24 @@
 ﻿using System.Text.Json;
 using Movies.Api.Sdk;
+using Movies.Contracts.Requests;
 using Refit;
 
 var moviesApi = RestService.For<IMoviesApi>("https://localhost:7163");
 
-var movie = await moviesApi.GetMoviesAsync("nick-the-greek-2023");
+// var movie = await moviesApi.GetMovieAsync("nick-the-greek-2023");
 
-Console.WriteLine(JsonSerializer.Serialize(movie));
+var request = new GetAllMoviesRequest
+{
+    Title = null,
+    Year = null,
+    SortBy = null,
+    Page = 1,
+    PageSize = 3,
+};
+
+var movies = await moviesApi.GetAllMoviesAsync(request);
+
+foreach (var movieResponse in movies.Items)
+{
+    Console.WriteLine(JsonSerializer.Serialize(movieResponse));
+}
